6042584c3eb0fd5dfa97019ba167abcd36e5b105
Arnaud Nurdin <146205767+NURDIN-Arnaud-2326082b@users.noreply.github.com>
2025-11-07 14:25:39 +0100

    Revert "Merge branch 'dev' into modifs-inscription"
    This reverts commit 2f9e77a3b68ff3aaaf64f94ca3cc95ef6eccec35, reversing
    changes made to 4671369cf6da5d782154531506ab1396514261f6.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -15,23 +15,14 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(
-        Request $request,
-        UserPasswordHasherInterface $userPasswordHasher,
-        Security $security,
-        EntityManagerInterface $entityManager,
-        TagRepository $tagRepository,
-        SluggerInterface $slugger,
-        HttpClientInterface $httpClient
-    ): Response {
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager, TagRepository $tagRepository, SluggerInterface $slugger): Response
+    {
         $user = new User();
-
-        // Questions dynamiques
+        // Définir les questions dynamiques
         $dynamicQuestions = [
             'Pourquoi cette thématique de recherche vous intéresse-t-elle ?',
             'Pourquoi avez-vous souhaité être chercheur ?',
@@ -40,8 +31,8 @@ class RegistrationController extends AbstractController
             'Quelles sont les méthodologies de recherche que vous utilisez dans votre domaine d\'étude ?',
             'Qu\'est ce qui, d\'après vous, vous a amené(e) à faire de la recherche ?',
             'Comment vous définirirez vous en tant que chercheur?',
-            'Pensez-vous que ce choix ait un lien avec  un évènement de votre biographie ? (rencontre, auteur, environnement personnel, professionnel ....) et si oui pouvez-vous brièvement le/la décrire ?',
-            'Pouvez-vous nous raconter qu\'est ce qui a motivé le choix  de vos thématiques de recherche ?',
+            'Pensez-vous que ce choix ait un lien avec  un évènement de votre biographie ? (rencontre, auteur, environnement personnel, professionnel ....) et si oui pouvez-vous brièvement le/la décrire ?',
+            'Pouvez-vous nous raconter qu\'est ce qui a motivé le choix  de vos thématiques de recherche ?',
             'Comment vos expériences personnelles ont-elles influencé votre choix de carrière et vos recherches en sciences humaines et sociales ?',
             'En quelques mots, en tant que chercheur(se) qu\'est ce qui vous anime ?',
             'Si vous deviez choisir 4 auteurs qui vous ont marquée, quels seraient-ils?',
@@ -59,99 +50,80 @@ class RegistrationController extends AbstractController
 
         $tags = $tagRepository->findAllOrderedByName();
 
+        // Créer le formulaire et passer les questions dynamiques comme option
         $form = $this->createForm(RegistrationFormType::class, $user, [
             'dynamic_questions' => $dynamicQuestions,
             'min_questions_required' => $minQuestionsRequired,
             'taggable_questions' => $taggableQuestions,
             'taggable_min_choices' => $taggableMinChoices,
-            'tags' => $tags,
+            'tags' => $tags, // Passer les tags au formulaire
         ]);
 
         $form->handleRequest($request);
 
-        if ($form->isSubmitted()) {
-            $recaptchaResponse = $request->request->get('g-recaptcha-response');
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var string $plainPassword */
+            $plainPassword = $form->get('plainPassword')->getData();
 
-            if (!$recaptchaResponse) {
-                $this->addFlash('error', 'Veuillez confirmer que vous n’êtes pas un robot.');
-                return $this->redirectToRoute('app_register');
-            }
+            // encode the plain password
+            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
 
-            // Appel l’API Google pour valider le token
-            $response = $httpClient->request('POST', 'https://www.google.com/recaptcha/api/siteverify', [
-                'body' => [
-                    'secret' => $_ENV['6Lfx-gIsAAAAADs2WiCDrn0kn74HyGrdVtLb637C'],
-                    'response' => $recaptchaResponse,
-                    'remoteip' => $request->getClientIp(),
-                ],
-            ]);
+            // Gestion de l'upload de la photo de profil
+            $profileImageFile = $form->get('profileImageFile')->getData();
+            if ($profileImageFile) {
+                $originalFilename = pathinfo($profileImageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$profileImageFile->guessExtension();
 
-            $data = $response->toArray();
-
-            if (!$data['success'] || ($data['score'] ?? 0) < 0.5) {
-                $this->addFlash('error', 'La vérification reCAPTCHA a échoué. Veuillez réessayer.');
-                return $this->redirectToRoute('app_register');
-            }
-
-            if ($form->isValid()) {
-                /** @var string $plainPassword */
-                $plainPassword = $form->get('plainPassword')->getData();
-
-                $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
-
-                // Gestion de l'upload de la photo
-                $profileImageFile = $form->get('profileImageFile')->getData();
-                if ($profileImageFile) {
-                    $originalFilename = pathinfo($profileImageFile->getClientOriginalName(), PATHINFO_FILENAME);
-                    $safeFilename = $slugger->slug($originalFilename);
-                    $newFilename = $safeFilename.'-'.uniqid().'.'.$profileImageFile->guessExtension();
-
-                    try {
-                        $profileImageFile->move(
-                            $this->getParameter('kernel.project_dir').'/assets/profile_images',
-                            $newFilename
-                        );
-                    } catch (FileException $e) {
-                        $this->addFlash('danger', "Erreur lors de l'upload de la photo de profil.");
-                    }
-                    $user->setProfileImage($newFilename);
+                try {
+                    $profileImageFile->move(
+                        $this->getParameter('kernel.project_dir').'/assets/profile_images',
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    $this->addFlash('danger', "Erreur lors de l'upload de la photo de profil.");
                 }
+                $user->setProfileImage($newFilename);
+            }
 
-                $entityManager->persist($user);
+            $entityManager->persist($user);
 
-                // Sauvegarde des questions utilisateur
-                $questions = $form->get('userQuestions')->getData();
-                foreach ($questions as $index => $questionText) {
-                    if (!empty($questionText)) {
+            // Handle user questions
+            $questions = $form->get('userQuestions')->getData();
+
+            foreach ($questions as $index => $questionText) {
+            if (!empty($questionText)) {
+                $userQuestion = new UserQuestions();
+                $userQuestion->setUser($user);
+                $userQuestion->setQuestion('Question ' . $index);
+                $userQuestion->setAnswer($questionText);
+
+                $entityManager->persist($userQuestion);
+            }
+            }
+
+            $taggableRaw = $form->get('taggableQuestions')->getData() ?? [];
+
+            foreach ($taggableRaw as $index => $ids) {
+                if (!is_array($ids)) {
+                    $ids = $ids ? [$ids] : [];
+                }
+                foreach ($ids as $tagId) {
+                    $tag = $tagRepository->find($tagId);
+                    if ($tag) {
                         $userQuestion = new UserQuestions();
                         $userQuestion->setUser($user);
-                        $userQuestion->setQuestion('Question ' . $index);
-                        $userQuestion->setAnswer($questionText);
+                        $userQuestion->setQuestion("Taggable Question $index");
+                        $userQuestion->setAnswer($tag->getName());
                         $entityManager->persist($userQuestion);
                     }
                 }
-
-                $taggableRaw = $form->get('taggableQuestions')->getData() ?? [];
-                foreach ($taggableRaw as $index => $ids) {
-                    if (!is_array($ids)) {
-                        $ids = $ids ? [$ids] : [];
-                    }
-                    foreach ($ids as $tagId) {
-                        $tag = $tagRepository->find($tagId);
-                        if ($tag) {
-                            $userQuestion = new UserQuestions();
-                            $userQuestion->setUser($user);
-                            $userQuestion->setQuestion("Taggable Question $index");
-                            $userQuestion->setAnswer($tag->getName());
-                            $entityManager->persist($userQuestion);
-                        }
-                    }
-                }
-
-                $entityManager->flush();
-
-                return $security->login($user, 'form_login', 'main');
             }
+
+            $entityManager->flush();
+
+            // do anything else you need here, like send an email
+            return $security->login($user, 'form_login', 'main');
         }
 
         return $this->render('registration/register.html.twig', [
