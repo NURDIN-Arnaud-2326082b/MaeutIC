a4db7ec68935823a4415e9c9ae2840ef0d4015a3
LECUYER-Theotime-2326067l <theotime.lecuyer@etu.univ-amu.fr>
2025-11-07 15:13:40 +0100

    Encryption system for messages with encryption key

--- src/Entity/Message.php
+++ src/Entity/Message.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\MessageRepository;
+use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: MessageRepository::class)]
@@ -14,25 +15,77 @@ class Message
     private ?int $id = null;
 
     #[ORM\Column(type: 'text')]
-    private ?string $content = null;
+    private ?string $encryptedContent = null;
 
     #[ORM\ManyToOne(targetEntity: User::class)]
     #[ORM\JoinColumn(nullable: true, onDelete: "SET NULL")]
     private ?User $sender = null;
 
     #[ORM\Column(type: 'datetime')]
-    private ?\DateTimeInterface $sentAt = null;
+    private ?DateTimeInterface $sentAt = null;
 
     #[ORM\ManyToOne(targetEntity: Conversation::class, inversedBy: 'messages')]
-    private ?Conversation $conversation = null; // null = message public (chat général)
+    private ?Conversation $conversation = null;
 
-    public function getId(): ?int { return $this->id; }
-    public function getContent(): ?string { return $this->content; }
-    public function setContent(string $content): static { $this->content = $content; return $this; }
-    public function getSender(): ?User { return $this->sender; }
-    public function setSender(User $sender): static { $this->sender = $sender; return $this; }
-    public function getSentAt(): ?\DateTimeInterface { return $this->sentAt; }
-    public function setSentAt(\DateTimeInterface $sentAt): static { $this->sentAt = $sentAt; return $this; }
-    public function getConversation(): ?Conversation { return $this->conversation; }
-    public function setConversation(?Conversation $conversation): static { $this->conversation = $conversation; return $this; }
+    private ?string $content = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEncryptedContent(): ?string
+    {
+        return $this->encryptedContent;
+    }
+
+    public function setEncryptedContent(string $encryptedContent): static
+    {
+        $this->encryptedContent = $encryptedContent;
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+        return $this;
+    }
+
+    public function getSender(): ?User
+    {
+        return $this->sender;
+    }
+
+    public function setSender(User $sender): static
+    {
+        $this->sender = $sender;
+        return $this;
+    }
+
+    public function getSentAt(): ?DateTimeInterface
+    {
+        return $this->sentAt;
+    }
+
+    public function setSentAt(DateTimeInterface $sentAt): static
+    {
+        $this->sentAt = $sentAt;
+        return $this;
+    }
+
+    public function getConversation(): ?Conversation
+    {
+        return $this->conversation;
+    }
+
+    public function setConversation(?Conversation $conversation): static
+    {
+        $this->conversation = $conversation;
+        return $this;
+    }
 }
