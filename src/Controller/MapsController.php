2807ff63c984c07ce776c73d492b58e8731a8880
FOURNIER Quentin <quentin.fournier@etu.univ-amu.fr>
2025-11-12 17:07:46 +0100

    affichage de liens entre amis (map)
    les amis s'affichent maintenant sur la map reliés par des pointillés jusqu'a la personne connectée. Les bulles sont de couleurs différentes.

--- src/Controller/MapsController.php
+++ src/Controller/MapsController.php
@@ -7,26 +7,50 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Repository\UserRepository;
+use App\Service\NetworkService;
 
-final class MapsController extends AbstractController{
+final class MapsController extends AbstractController
+{
     #[Route('/maps', name: 'app_maps')]
-    public function index(UserRepository $userRepository): Response
+    public function index(UserRepository $userRepository, NetworkService $networkService): Response
     {
+        $currentUser = $this->getUser();
         $users = $userRepository->findAll();
+        
+        // Récupérer les IDs des amis si l'utilisateur est connecté
+        $friendIds = [];
+        if ($currentUser) {
+            $friends = $networkService->getUserNetwork($currentUser);
+            $friendIds = array_map(fn($friend) => $friend->getId(), $friends);
+        }
+
         return $this->render('maps/index.html.twig', [
             'controller_name' => 'MapsController',
             'users' => $users,
+            'friend_ids' => $friendIds,
+            'current_user_id' => $currentUser ? $currentUser->getId() : null,
         ]);
     }
 
     #[Route('/maps/filter', name: 'app_user_map_filter')]
-    public function filter(Request $request, UserRepository $userRepository): Response
+    public function filter(Request $request, UserRepository $userRepository, NetworkService $networkService): Response
     {
+        $currentUser = $this->getUser();
         $tagIds = $request->query->all('tags');
-        // À adapter selon votre logique métier pour filtrer les users par tags de la première question taggable
+        
         $users = $userRepository->findByTaggableQuestion1Tags($tagIds);
+        
+        // Récupérer les IDs des amis si l'utilisateur est connecté
+        $friendIds = [];
+        if ($currentUser) {
+            $friends = $networkService->getUserNetwork($currentUser);
+            $friendIds = array_map(fn($friend) => $friend->getId(), $friends);
+        }
+
         return $this->render('maps/_bubbles.html.twig', [
             'users' => $users,
+            'friend_ids' => $friendIds,
+            'current_user_id' => $currentUser ? $currentUser->getId() : null,
         ]);
     }
 }
