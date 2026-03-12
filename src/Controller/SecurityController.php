e6abd8499774dad8ff77439386a1cac2de933c10
Arnaud Nurdin <arnaud.nurdin@etu.univ-amu.fr>
2026-03-12 14:15:44 +0100

    fix react

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,42 +12,10 @@ namespace App\Controller;
 
 use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class SecurityController extends AbstractController
 {
-    /**
-     * Affiche le formulaire de connexion et gère l'authentification
-     *
-     * @param Request $request La requête HTTP
-     * @param AuthenticationUtils $authenticationUtils Utilitaire pour récupérer les erreurs d'authentification
-     * @param HttpClientInterface $httpClient Client HTTP pour les appels externes
-     * @return Response La page de connexion rendue
-     */
-    #[Route(path: '/login', name: 'app_login')]
-    public function login(Request $request, AuthenticationUtils $authenticationUtils, HttpClientInterface $httpClient): Response
-    {
-        if ($this->getUser()) {
-            return $this->redirectToRoute('app_home');
-        }
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error,
-
-
-        ]);
-    }
-
     /**
      * Gère la déconnexion de l'utilisateur
      *
