1c7ec54af0e8156682c6044581a88e4fb700b192
LECUYER-Theotime-2326067l <theotime.lecuyer@etu.univ-amu.fr>
2026-03-19 16:48:03 +0100

    fix forum fixtures

--- src/DataFixtures/ForumFixtures.php
+++ src/DataFixtures/ForumFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Forum;
+use DateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,13 +12,13 @@ class ForumFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $forumsData = [
-            ['title' => 'Divers', 'body' => 'Discussions générales, actualités de la recherche et échanges informels entre membres du réseau.', 'special' => null],
+            ['title' => 'Divers', 'body' => 'Discussions générales, actualités de la recherche et échanges informels entre membres du réseau.', 'special' => 'general'],
             ['title' => 'Administratif', 'body' => 'Entraide sur les démarches, financements, bourses, contrats doctoraux et qualification.', 'special' => 'administratif'],
             ['title' => 'Méthodologie quantitative', 'body' => 'Questions sur les statistiques, R, Python, SPSS, passation de questionnaires et échantillonnage.', 'special' => 'methodology'],
             ['title' => 'Méthodologie qualitative', 'body' => 'Échanges autour des grilles d\'entretien, de l\'observation participante et des logiciels comme NVivo.', 'special' => 'methodology'],
             ['title' => 'Méthodologie mixte', 'body' => 'Comment trianguler vos données qualitatives et quantitatives efficacement ?', 'special' => 'methodology'],
-            ['title' => 'Auteurs', 'body' => 'Débats et partages de fiches de lecture sur les grands théoriciens et auteurs contemporains.', 'special' => null],
-            ['title' => 'Oeuvres', 'body' => 'Recommandations d\'ouvrages, critiques littéraires et revues de littérature.', 'special' => null],
+            ['title' => 'Auteurs', 'body' => 'Débats et partages de fiches de lecture sur les grands théoriciens et auteurs contemporains.', 'special' => 'general'],
+            ['title' => 'Oeuvres', 'body' => 'Recommandations d\'ouvrages, critiques littéraires et revues de littérature.', 'special' => 'general'],
             ['title' => 'Détente', 'body' => 'La pause café des chercheurs. Anecdotes de terrain, memes académiques et décompression.', 'special' => 'detente']
         ];
 
@@ -25,11 +26,8 @@ class ForumFixtures extends Fixture
             $forum = new Forum();
             $forum->setTitle($data['title'])
                 ->setBody($data['body'])
-                ->setLastActivity(new \DateTime());
-
-            if ($data['special']) {
-                $forum->setSpecial($data['special']);
-            }
+                ->setLastActivity(new DateTime())
+                ->setSpecial($data['special']); // On force la valeur (ex: 'general') au lieu de NULL
 
             $manager->persist($forum);
             $this->addReference("forum" . ($i + 1), $forum);
